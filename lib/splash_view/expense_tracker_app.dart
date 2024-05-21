1fde97fd7696b5a5d20c9b4225a564c112d253f4
ib <132789212+ibousv@users.noreply.github.com>
2024-05-21 12:11:32 +0000

    init

---
+++ lib/splash_view/expense_tracker_app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+  class ExpenseTrackerApp extends StatefulWidget {
+  const ExpenseTrackerApp({super.key});
+
+  @override
+  State<ExpenseTrackerApp> createState() => _State();
+}
+
+class _State extends State<ExpenseTrackerApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
