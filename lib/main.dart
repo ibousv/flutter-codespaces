1da30cdddf4b8d964d2ab2b7ad515480f95e8d97
ib <132789212+ibousv@users.noreply.github.com>
2024-05-21 12:54:27 +0000

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,28 +11,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-       
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return GetMaterialApp();
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
--- lib/splash_view/expense_tracker_app.dart
+++ lib/splash_view/expense_tracker_app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
   class ExpenseTrackerApp extends StatefulWidget {
   const ExpenseTrackerApp({super.key});
 
